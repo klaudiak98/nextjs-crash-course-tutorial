48b9e0f21f78dbc667fc9e64d054a79684467201
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2023-09-17 13:22:09 +0200

    Add some styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import Logo from './logo.png';
 
 export default function Navbar() {
   return (
         <nav>
+            <Image
+                src={Logo}
+                alt='Helpdesk Logo'
+                width={70}
+                quality={100}
+                placeholder='blur'
+            />
             <h1>Helpdesk</h1>
             <Link href='/'>Home</Link>
             <Link href='/tickets'>Tickets</Link>
